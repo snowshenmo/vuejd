7e48fd0e0c78dd74a13b1c7bd72d019ad4a6b5d6
snow <1174714964@qq.com>
2018-04-29 12:14:14 +0800

    add header

---
+++ src/components/jd-head/jd-head.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="header-box">
+		<div class="header">
+			<div class="lf">
+				<img src="./star.jpg" alt="收藏京东">
+				收藏京东
+			</div>
+			<ul class="rt">
+				<li>
+					您好，欢迎来到京东！
+					<a href="#">[登录]</a>
+					<a href="#">[免费注册]</a>
+				</li>
+				<li>
+					<b></b>
+					<a href="#">我的订单</a>
+				</li>
+				<li>
+					<b></b>
+					<a href="#">会员俱乐部</a>
+				</li>
+				<li>
+					<b></b>
+					<a href="#">企业频道</a>
+				</li>
+				<li>
+					<b></b>
+					<a href="#">手机京东</a>
+				</li>
+				<li>
+					<b></b>
+					<a href="#">客户服务</a>
+				</li>
+				<li>
+					<b></b>
+					<a href="#">网站导航</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+
+	}
+</script>
+
+<style scoped>
+	.header-box{
+		background-color: #f7f7f7;
+	}
+	.header{
+		width: 1211px;
+		height: 30px;
+		line-height: 30px;
+		margin: 0 auto;
+	}
+	.header img{
+		margin-top: 9px;
+		margin-right: 5px;
+	}
+</style>
